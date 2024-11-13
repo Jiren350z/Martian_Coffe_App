5c96291285ecaec3668bd5d66270c4154ea260e5
Jiren350z <diegomoralesv11@gmail.com>
2024-11-13 16:05:06 -0300

    se tiene lo de compartir recetas a traves de redes sociales

--- lib/pages/opinion.dart
+++ lib/pages/opinion.dart
@@ -3,7 +3,158 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 
+class FeedbackScreen extends StatefulWidget {
+  @override
+  _FeedbackScreenState createState() => _FeedbackScreenState();
+}
 
+class _FeedbackScreenState extends State<FeedbackScreen> {
+  Map<String, dynamic> questions = {};
+  Map<String, int> feedbackValues = {};
+  String userName = ''; // Cambiado a userName para capturar el nombre del usuario
+
+  @override
+  void initState() {
+    super.initState();
+    loadQuestions();
+  }
+
+  Future<void> loadQuestions() async {
+    final String response = await rootBundle.loadString('assets/json/questions.json');
+    final data = await json.decode(response);
+    setState(() {
+      questions = data;
+      questions.forEach((section, questionList) {
+        for (var question in questionList) {
+          feedbackValues[question['titulo']] = question['valor'];
+        }
+      });
+    });
+  }
+
+  void submitFeedback() async {
+    String feedbackContent = 'Nombre del usuario: $userName\n\n'; // Incluye el nombre del usuario
+    questions.forEach((section, questionList) {
+      feedbackContent += '$section:\n';
+      for (var question in questionList) {
+        String title = question['titulo'];
+        int rating = feedbackValues[title] ?? 0;
+        feedbackContent += '$title - Calificación: $rating estrellas\n';
+      }
+      feedbackContent += '\n';
+    });
+
+    final Email email = Email(
+      body: feedbackContent,
+      subject: 'Retroalimentación de la App',
+      recipients: ['diegomoralesv11@gmail.com'],
+    );
+
+    try {
+      await FlutterEmailSender.send(email);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('¡Retroalimentación enviada exitosamente!')),
+      );
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al enviar la retroalimentación')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Tu Opinión')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Nombre'),
+              onChanged: (value) {
+                setState(() {
+                  userName = value; // Guarda el nombre ingresado por el usuario
+                });
+              },
+            ),
+            Expanded(
+              child: ListView(
+                children: questions.entries.map((entry) {
+                  String section = entry.key;
+                  List questionList = entry.value;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        section,
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      ...questionList.map((question) {
+                        String title = question['titulo'];
+                        String minDesc = question['min'];
+                        String maxDesc = question['max'];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10),
+                            Text(
+                              title,
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                            Slider(
+                              min: 0,
+                              max: 5,
+                              divisions: 5,
+                              label: '${feedbackValues[title]} estrellas',
+                              value: feedbackValues[title]?.toDouble() ?? 0,
+                              activeColor: Colors.blue,
+                              inactiveColor: Colors.blueGrey,
+                              onChanged: (value) {
+                                setState(() {
+                                  feedbackValues[title] = value.toInt();
+                                });
+                              },
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              '$minDesc',
+                              style: TextStyle(
+                                fontSize: 13,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              '$maxDesc',
+                              style: TextStyle(
+                                fontSize: 13,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                          ],
+                        );
+                      }).toList(),
+                      SizedBox(height: 20),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: submitFeedback,
+              child: Text('Enviar retroalimentación'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class FeedbackScreen extends StatefulWidget {
   @override
   _FeedbackScreenState createState() => _FeedbackScreenState();
@@ -125,162 +276,6 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
                               style: TextStyle(
                                 fontSize: 13,
                               ),
-                            ),
-                            SizedBox(height: 10),
-                          ],
-                        );
-                      }).toList(),
-                      SizedBox(height: 20),
-                    ],
-                  );
-                }).toList(),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: submitFeedback,
-              child: Text('Enviar retroalimentación'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/*
-class FeedbackScreen extends StatefulWidget {
-  @override
-  _FeedbackScreenState createState() => _FeedbackScreenState();
-}
-
-class _FeedbackScreenState extends State<FeedbackScreen> {
-  Map<String, dynamic> questions = {};
-  Map<String, int> feedbackValues = {};
-  String userId = '';
-
-  @override
-  void initState() {
-    super.initState();
-    loadQuestions();
-  }
-
-  Future<void> loadQuestions() async {
-    final String response = await rootBundle.loadString('assets/json/questions.json');
-    final data = await json.decode(response);
-    setState(() {
-      questions = data;
-      questions.forEach((section, questionList) {
-        for (var question in questionList) {
-          feedbackValues[question['titulo']] = question['valor'];
-        }
-      });
-    });
-  }
-
-  void submitFeedback() async {
-    String feedbackContent = 'ID de usuario: $userId\n\n';
-    questions.forEach((section, questionList) {
-      feedbackContent += '$section:\n';
-      for (var question in questionList) {
-        String title = question['titulo'];
-        int rating = feedbackValues[title] ?? 0;
-        feedbackContent += '$title - Calificación: $rating estrellas\n';
-      }
-      feedbackContent += '\n';
-    });
-
-    final Email email = Email(
-      body: feedbackContent,
-      subject: 'Retroalimentación de la App',
-      recipients: ['diegomoralesv11@gmail.com'],
-    );
-
-    try {
-      await FlutterEmailSender.send(email);
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('¡Retroalimentación enviada exitosamente!')),
-      );
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error al enviar la retroalimentación')),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Tu Opinión')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /*
-            TextField(
-              decoration: InputDecoration(labelText: 'ID de usuario'),
-              onChanged: (value) {
-                setState(() {
-                  userId = value;
-                });
-              },
-            ),
-            */
-            Expanded(
-              child: ListView(
-                children: questions.entries.map((entry) {
-                  String section = entry.key;
-                  List questionList = entry.value;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        section,
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      ...questionList.map((question) {
-                        String title = question['titulo'];
-                        String minDesc = question['min'];
-                        String maxDesc = question['max'];
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 10),
-                            Text(
-                            title,
-                              style: TextStyle(
-                              fontSize: 15, // Cambia este valor al tamaño que prefieras
-                              //fontWeight: FontWeight.bold, // Opcional: para hacer el texto en negrita
-                              ),
-                            ),
-                            
-                            Slider(
-                              min: 0,
-                              max: 5,
-                              divisions: 5,
-                              label: '${feedbackValues[title]} estrellas',
-                              value: feedbackValues[title]?.toDouble() ?? 0,
-                              activeColor: Colors.blue,
-                              inactiveColor: Colors.blueGrey,
-                              onChanged: (value) {
-                                setState(() {
-                                  feedbackValues[title] = value.toInt();
-                                });
-                              },
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              '$minDesc',
-                              style: TextStyle(
-                              fontSize: 13, // Cambia este valor al tamaño que prefieras   
-                              ),                              
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              '$maxDesc',
-                              style: TextStyle(
-                              fontSize: 13, // Cambia este valor al tamaño que prefieras 
-                              ),                              
                             ),
                             SizedBox(height: 10),
                           ],
--- lib/pages/recipe_detail_page.dart
+++ lib/pages/recipe_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:martian_cofee_app/models/recipe_class.dart';
 import 'dart:io';
 import 'package:martian_cofee_app/utils/datebase_helper.dart';
+import 'package:share_plus/share_plus.dart';
 
 class RecipeDetailPage extends StatefulWidget {
   final RecipeNew recipe;
@@ -64,30 +65,35 @@ class RecipeDetailPageState extends State<RecipeDetailPage> {
     });
   }
 
-  /*
-  Future<void> _saveRecipe() async {
-    setState(() {
-      widget.recipe.name = nameController.text;
-      widget.recipe.preparationTime = int.tryParse(prepTimeController.text) ?? widget.recipe.preparationTime;
-      widget.recipe.ingredients = ingredientsController.text.split(',').map((e) => e.trim()).toList();
-      widget.recipe.utensils = utensilsController.text.split(',').map((e) => e.trim()).toList();
-      widget.recipe.preparation = preparationController.text;
-    });
+  void shareRecipe() {
+    final String recipeDetails = '''
+      ${widget.recipe.name}
+      
+      Duración total de la preparación: ${widget.recipe.preparationTime} minutos
+      
+      Ingredientes:
+      ${widget.recipe.ingredients.map((ingredient) => '- $ingredient').join('\n')}
+      
+      Utensilios:
+      ${widget.recipe.utensils.map((utensil) => '- $utensil').join('\n')}
+      
+      Preparación:
+      ${widget.recipe.preparation}
+    ''';
 
-    // Lógica para actualizar la receta en la base de datos.
-    await DatabaseHelper().updateRecipe(widget.recipe);
-
-    setState(() {
-      isEditing = false;
-    });
+    Share.share(recipeDetails, subject: 'Receta: ${widget.recipe.name}');
   }
-  */
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.recipe.name),
         actions: [
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: shareRecipe,
+          ),
           if (!isEditing)
             IconButton(
               icon: Icon(Icons.edit),
@@ -159,19 +165,7 @@ class RecipeDetailPageState extends State<RecipeDetailPage> {
                               },
                             )
                           : const Placeholder(fallbackHeight: 200),
-                    /*
-                    widget.recipe.imageUrl.isNotEmpty
-                        ? Image.file(
-                            File(widget.recipe.imageUrl),
-                            fit: BoxFit.cover,
-                            height: 200,
-                            width: double.infinity,
-                            errorBuilder: (context, error, stackTrace) {
-                              return const Text('Error al cargar la imagen');
-                            },
-                          )
-                        : const Placeholder(fallbackHeight: 200),
-                        */
+ 
                     const SizedBox(height: 10),
                     Text('Duración total de la preparación: ${widget.recipe.preparationTime}',
                         style: const TextStyle(fontSize: 16)),
@@ -193,161 +187,3 @@ class RecipeDetailPageState extends State<RecipeDetailPage> {
   }
 }
 
-//original y funcional
-/*
-class RecipeDetailPage extends StatefulWidget {
-  final RecipeNew recipe;
-  
-
-  const RecipeDetailPage({super.key, required this.recipe});
-
-  @override
-  RecipeDetailPageState createState() => RecipeDetailPageState();
-}
-
-class RecipeDetailPageState extends State<RecipeDetailPage> {
-  bool isFavorite = false;
-  int userRating = 0;
-  //final TextEditingController comment = TextEditingController();
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.recipe.name),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.share),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-                widget.recipe.imageUrl.isNotEmpty
-                ? Image.file(
-                    File(widget.recipe.imageUrl),
-                    fit: BoxFit.cover,
-                    height: 200,
-                    width: double.infinity,
-                    errorBuilder: (context, error, stackTrace) {
-                      return const Text('Error al cargar la imagen');
-                    },
-                  )
-                : const Placeholder(fallbackHeight: 200),
-
-              const SizedBox(height: 10),
-              // Fecha de registro
-              Text(
-                'Fecha de registro: ${widget.recipe.registrationDate}',
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 10),
-
-              // Duración total de la preparación
-              Text(
-                'Duración total de la preparación: ${widget.recipe.preparationTime}',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-
-              // Ingredientes
-              const Text('Ingredientes:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: widget.recipe.ingredients.map((ingredient) {
-                  return Text('- $ingredient', style: const TextStyle(fontSize: 16));
-                }).toList(),
-              ),
-              const SizedBox(height: 10),
-
-              // Utensilios
-              const Text('Utensilios:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: widget.recipe.utensils.map((utensil) {
-                  return Text('- $utensil', style: const TextStyle(fontSize: 16));
-                }).toList(),
-              ),
-              const SizedBox(height: 10),
-
-              // Preparación
-              const Text('Preparación:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              Text(widget.recipe.preparation, style: const TextStyle(fontSize: 16)),
-              const SizedBox(height: 10),
-
-              //productos
-              /*
-              // Productos relacionados (Scroll horizontal)
-              const Text('Productos relacionados:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 10),
-              
-              SizedBox(
-                height: 120,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: widget.recipe.ingredients.length,
-                  itemBuilder: (context, index) {
-                    final product = widget.recipe.ingredients[index];
-                    return Card(
-                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                      child: SizedBox(
-                        width: 200,
-                        child: ListTile(
-                          leading: Image.asset(
-                            product.imageOfProduct,
-                            width: 80,
-                            height: 80,
-                            fit: BoxFit.cover,
-                          ),
-                          title: Text(
-                            product.name,
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Precio: \$${product.price}', style: const TextStyle(color: Colors.green)),
-                              Text('Stock: ${product.stock}'),
-                              Row(
-                                children: List.generate(5, (starIndex) {
-                                  return Icon(
-                                    starIndex < product.rating ? Icons.star : Icons.star_border,
-                                    color: Colors.amber,
-                                    size: 20,
-                                  );
-                                }),
-                              ),
-                            ],
-                          ),
-                          trailing: const Icon(Icons.arrow_forward),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ProductWidget(product: product),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 20),
-              */
-              
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-*/
